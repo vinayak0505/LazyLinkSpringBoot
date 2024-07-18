bbdbc9baf183e5a4ab50acd556135311bcee57cc
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-07-18 13:16:32 +0530

    project setup

---
+++ src/main/java/com/lazyengineer/lazylinkspringboot/LazyLinkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.lazyengineer.lazylinkspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyLinkSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyLinkSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lazyengineer/lazylinkspringboot/LazyLinkSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazyengineer.lazylinkspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyLinkSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
